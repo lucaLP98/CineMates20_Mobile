020da4aee53c1fabf990a743744c203788619874
Luca Pastore <65617339+lucaLP98@users.noreply.github.com>
2021-09-26 03:10:59 +0200

    Project created

---
+++ app/src/main/java/it/ingsw/cinemates20_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package it.ingsw.cinemates20_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
